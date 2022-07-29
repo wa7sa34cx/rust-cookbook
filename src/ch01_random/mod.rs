2dea1aa23727e18100d60b3691c7ea7c68b26fee
Alex Black <wa7@sa34.cx>
2022-07-29 23:13:23 +0300

    with range

--- src/ch01_random/mod.rs
+++ src/ch01_random/mod.rs
@@ -6,10 +6,18 @@ pub fn random_numbers() {
 
     let n1: u8 = rng.gen();
     let n2: u16 = rng.gen();
-    
+
     println!("Random u8: {}", n1);
     println!("Random u16: {}", n2);
     println!("Random u32: {}", rng.gen::<u32>());
     println!("Random i32: {}", rng.gen::<i32>());
     println!("Random float: {}", rng.gen::<f64>());
+}
+
+// Generate random numbers within a range
+pub fn within_a_range() {
+    let mut rng = rand::thread_rng();
+    
+    println!("Integer: {}", rng.gen_range(0..10));
+    println!("Float: {}", rng.gen_range(0.0..10.0));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#![allow(dead_code, unused_imports)]
+
 mod ch01_random;
 
 // use ch01_random;
 
 fn main() {
-    ch01_random::random_numbers();
+    // ch01_random::random_numbers();
+    ch01_random::within_a_range();
 }
